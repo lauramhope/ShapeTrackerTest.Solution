2dee300499c334370233ffddd3229e7fa6c7bccd
Laura Hope <ssteamlaura@gmail.com>
2023-04-29 16:46:49 -0700

    Add functionalility and passing test.

---
+++ ModelTests/TriangleTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeTracker.Models;
+
+namespace ShapeTracker.Tests
+{
+  [TestClass]
+  public class TriangleTests
+  {
+
+     [TestMethod]
+    public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
+    {
+      Triangle newTriangle = new Triangle(2); // Updated!
+      Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+    }
+
+        [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    {
+      // Arrange
+      int length1 = 3;
+      Triangle newTriangle = new Triangle(length1);
+      // Act
+      int result = newTriangle.Side1;
+      // Assert
+      Assert.AreEqual(length1, result);
+    }
+
+  }
+}
